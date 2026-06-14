427e94edf38e4837bdbac84c1ca2052170220d94
AxelS27 <farrellaxel2006@gmail.com>
2026-06-15 06:39:01 +0700

    feat: rename application to Liem Microservice and bump version to 0.1.4

--- src/main/index.js
+++ src/main/index.js
@@ -72,10 +72,10 @@ function createWindow() {
 
 function createTray() {
   tray = new Tray(getTrayIconPath())
-  tray.setToolTip('Liem Control Panel')
+  tray.setToolTip('Liem Microservice')
   tray.setContextMenu(
     Menu.buildFromTemplate([
-      { label: 'Open Liem', click: () => mainWindow.show() },
+      { label: 'Open Liem Microservice', click: () => mainWindow.show() },
       { type: 'separator' },
       { label: 'Quit', click: () => app.exit(0) }
     ])
